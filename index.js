906e76dcaad7bd1e24404dd81773f8dd8b988f31
Muhammad Fadly <davit9747@gmail.com>
2023-06-07 19:54:49 +0700

    cors

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 const userRouter = require("./routes/user");
 const kategoriRouter = require("./routes/kategori");
 const lokasiRouter = require("./routes/lokasi");
 const kelasRouter = require("./routes/kelas");
+const landingRouter = require("./routes/landingpage");
+
+const URL = `/api/v1`;
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -40,6 +54,7 @@ app.use("/user", userRouter);
 app.use("/kategori", kategoriRouter);
 app.use("/lokasi", lokasiRouter);
 app.use("/kelas", kelasRouter);
+app.use(`${URL}/landingpage`, landingRouter);
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log("Server started on port 3000");
