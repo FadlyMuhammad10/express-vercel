04288fe154485b8212b75cc2810980ea0be2b560
Muhammad Fadly <davit9747@gmail.com>
2023-06-09 18:25:48 +0700

    update cors

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var cors = require("cors");
 var path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
