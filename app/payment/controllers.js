5e730acae8cf6b26131114d1fd4ca562fc572c16
Muhammad Fadly <davit9747@gmail.com>
2023-10-02 21:43:06 +0700

    update

--- app/payment/controllers.js
+++ app/payment/controllers.js
@@ -33,7 +33,7 @@ module.exports = {
         transaction_status: webhookData.transaction_status,
         payment_type: webhookData.payment_type,
         fraud_status: webhookData.fraud_status,
-        email: webhookData.user_id,
+        email: req.user.email,
         // webhookData.status_code === "200" ? "settlement" : "pending",
       });
       await transaction.save();
