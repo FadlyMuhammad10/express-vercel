565bb32a011f9b4b9906eb8c2c61f95e5b1571ff
Muhammad Fadly <davit9747@gmail.com>
2023-10-02 11:32:43 +0700

    update controller webhook untuk mengubah status transaction tanpa duplikat data

--- app/payment/controllers.js
+++ app/payment/controllers.js
@@ -14,6 +14,19 @@ module.exports = {
   webhook: async (req, res) => {
     try {
       const webhookData = req.body;
+      // Cari apakah data transaksi dengan order_id yang sama sudah ada dalam database
+      const existingTransaction = await Transaction.findOne({
+        order_id: webhookData.order_id,
+      });
+      if (existingTransaction) {
+        // Jika data sudah ada, tidak perlu memasukkan data baru ke database
+        existingTransaction.transaction_status =
+          webhookData.status_code === "200" ? "success" : "failure";
+        await existingTransaction.save();
+        return res
+          .status(200)
+          .send("Webhook dari Midtrans diterima (status transaksi diperbarui)");
+      }
       const transaction = new Transaction({
         order_id: webhookData.order_id,
         transaction_id: webhookData.transaction_id,
