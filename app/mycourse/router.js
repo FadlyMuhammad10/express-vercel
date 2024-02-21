6f8a120f9e83f5a3520ca1b89695b38a1b08a228
Muhammad Fadly <davit9747@gmail.com>
2024-02-21 20:39:08 +0700

    delete my course

--- app/mycourse/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { create } = require("./controllers");
-const auth = require("../../middleware/auth");
-
-router.post("/create", auth, create);
-
-module.exports = router;
