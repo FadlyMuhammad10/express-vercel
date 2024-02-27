ca3f8d3a8403c3fd57763583f23c36b07a9fbef5
Muhammad Fadly <davit9747@gmail.com>
2024-02-27 16:21:35 +0700

    update router

--- app/payment/router.js
+++ app/payment/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { payment, webhook } = require("./controllers");
 const auth = require("../../middleware/auth");
 
-router.post("/payment", auth, payment);
+router.post("/payment", payment);
 router.post("/webhook", auth, webhook);
 
 module.exports = router;
