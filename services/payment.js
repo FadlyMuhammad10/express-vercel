8c28219ced1a523016e9faf6104c0c626c87f523
Muhammad Fadly <davit9747@gmail.com>
2024-02-29 21:23:06 +0700

    update payment

--- services/payment.js
+++ services/payment.js
@@ -1,5 +1,4 @@
 const uuid = require("uuid");
-const MyCourse = require("../app/myCourse/model");
 
 const midtransClient = require("midtrans-client");
 // Create Snap API instance
@@ -14,37 +13,6 @@ module.exports = {
       serverKey: process.env.server_key,
       clientKey: process.env.client_key,
     });
-
-    const { kelas_id } = req.body;
-    const my_course = await MyCourse.findOne({
-      user_id: body.user_id,
-    });
-    if (!my_course) {
-      // Jika tidak ada, buat dokumen course baru dengan kelas_id yang diberikan
-      const my_course = await MyCourse({
-        kelas_id: [kelas_id],
-        user_id: body.user_id,
-      });
-
-      await my_course.save();
-
-      return res
-        .status(201)
-        .json({ message: "success add My Course", my_course });
-    } else {
-      // Jika sudah ada, periksa apakah kelas_id sudah ada dalam array kelas_id
-      if (!my_course.kelas_id.includes(kelas_id)) {
-        // Jika belum ada, tambahkan kelas_id ke dalam array kelas_id
-        my_course.kelas_id.push(kelas_id);
-        await my_course.save();
-
-        return res
-          .status(200)
-          .json({ message: "Kelas berhasil ditambahkan ke course Anda." });
-      } else {
-        res.status(400).json({ error: "Kelas sudah ada dalam course Anda." });
-      }
-    }
 
     let parameter = {
       transaction_details: {
